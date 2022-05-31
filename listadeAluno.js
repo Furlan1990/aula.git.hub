18f2859271849a4b01682625b6e3a70ad715d0fe
Furlan1990 <105516264+Furlan1990@users.noreply.github.com>
2022-05-30 21:34:04 -0300

    Add files via upload

---
+++ listadeAluno.js
@@ -0,0 +1,31 @@
+let dataEvento = 12/06/2022
+    if (dataEvento < 31/05/2022) {
+        console.log ("Data inválida!")
+    } else {
+        console.log ("Evento cadastrado com sucesso!")
+    }
+
+let idade = 18
+    if (idade < 18) {
+        console.log ("Cadastro não permitido")
+    } else {
+        console.log ("Cadastro permitido!")
+    }
+
+let quantidadeAlunos = 50
+    if (quantidadeAlunos < 50) {
+        console.log ("Cadastro realizado!")
+    } else {
+        console.log ("Cadastro excedido!")
+    }
+
+let listaDeAlunos = ["Joana", "Joao", "Felipe", "Alyne"]
+
+if (listaAlunos.length < 50)
+{
+    console.log ("é possivel cadastrar!")
+}
+else
+{
+    console.log ("Não é possivel cadastrar")
+}
